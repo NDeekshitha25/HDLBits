9225d187ca1bbc24ecae61ccd33f12079b387738
N Deekshitha <ndeekshitha2807@gmail.com>
2026-03-10 19:14:46 +0530

    Added HDLBits "Always One" solution

--- 01_getting_started/step_one.v
+++ 01_getting_started/step_one.v
@@ -1,1 +1,9 @@
 
+// HDLBits Problem: Always One
+// Description: Build a circuit with no inputs and one output that always outputs logic high.
+
+module top_module( output one );
+
+    assign one = 1'b1;
+
+endmodule
