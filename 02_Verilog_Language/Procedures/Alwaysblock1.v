47a104fa4ca97bede6260cbfd79be86359babb50
N Deekshitha <ndeekshitha2807@gmail.com>
2026-03-17 21:37:30 +0530

    Add alwaysblock1 solution using assign and combinational always block

--- 02_Verilog_Language/Procedures/Alwaysblock1.v
+++ 02_Verilog_Language/Procedures/Alwaysblock1.v
@@ -1,5 +1,6 @@
 // HDLBits Problem: alwaysblock1
 // Implement an AND gate using both assign statement and always block
+//A combinational block (like an always @* or always_comb block) in Verilog is similar to a Python function in that it encapsulates a set of operations to produce an output based on inputs.
 
 module top_module(
     input a, 
